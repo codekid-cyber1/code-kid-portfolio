0eac6d8fe0440122875bd5138fd13c0c705c6c66
Abdulmujeeb Awodi <awodiabdulmujeeb@gmail.com>
2025-10-22 10:37:41 +0100

    add to my contact list

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react'
 import './Contact.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import mail_icon from '../../assets/mail_icon.svg'
-import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
+import x_icon from '../../assets/x_icon.svg'
+import instagram_icon from '../../assets/instagram_icon.svg'
+import linkedin_icon from '../../assets/linkedin_icon.svg'
+import github_icon from '../../assets/github_icon.svg'
 
 const Contact = () => {
 
@@ -63,16 +66,72 @@ const Contact = () => {
                 <p>I'm currently available to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
                 <div className="contact-details">
                     <div className="contact-detail">
+                      <a 
+                        href="mailto:awodiabdulmujeeb@gmail.com" 
+                        target="_blank"            
+                        rel="noopener noreferrer"
+                        aria-label="GitHub Profile"
+                      >
                         <img src={mail_icon} alt="" />
                         <p>awodiabdulmujeeb@gmail.com</p>
+                      </a>
+                        
                     </div>
                     <div className="contact-detail">
-                        <img src={call_icon} alt="" />
-                        <p>+234-813-378-0225</p>
+                      <a 
+                        href="tel:+2348133780225" 
+                        target="_blank"            
+                        rel="noopener noreferrer"
+                        aria-label="GitHub Profile"
+                      >
+                       <img src={call_icon} alt="" />
+                        <p>+234-813-378-0225</p> 
+                      </a>
                     </div>
                     <div className="contact-detail">
-                        <img src={location_icon} alt="" />
+                      <a 
+                        href="https://github.com/codekid-cyber1" 
+                        target="_blank"            
+                        rel="noopener noreferrer"
+                        aria-label="GitHub Profile"
+                      >
+                        <img src={github_icon} alt="" />
+                        <p>codekid-cyber1</p>
+                      </a>
+                    </div>
+                    <div className="contact-detail">
+                      <a 
+                        href="https://x.com/AbdulmujeebAwo1" 
+                        target="_blank"            
+                        rel="noopener noreferrer"
+                        aria-label="GitHub Profile"
+                      >
+                        <img src={x_icon} alt="" />
+                        <p>AbdulmujeebAwo1</p>
+                      </a>
+                        
+                    </div>
+                    <div className="contact-detail">
+                      <a 
+                        href="https://www.linkedin.com/in/abdulmujeeb-awodi-067a3227b/" 
+                        target="_blank"            
+                        rel="noopener noreferrer"
+                        aria-label="GitHub Profile"
+                      >
+                        <img src={linkedin_icon} alt="" />
+                        <p>Abdulmujeeb Awodi on LinkedIn</p>
+                      </a>
+                    </div>
+                    <div className="contact-detail">
+                      <a 
+                        href="https://github.com/codekid-cyber1" 
+                        target="_blank"            
+                        rel="noopener noreferrer"
+                        aria-label="GitHub Profile"
+                      >
+                        <img src={instagram_icon} alt="" />
                         <p>Kwara, Nigeria</p>
+                      </a>
                     </div>
                 </div>
             </div>
